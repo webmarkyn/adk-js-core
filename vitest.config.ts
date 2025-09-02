ee5aeab18f2225aae3b433f777a7b46ae8cac24d
Alexey Kalenkevich <kalenkevich@google.com>
2025-09-02 15:13:38 -0700

    Fix CI build
    Merge https://github.com/google/adk-js/pull/3
    
    Fix CI build:
    - added an alias for vitest config to point the src folder when importing the `@google/adk`
    - added few missing exports into index.ts
    - removed jasmine import from tests
    
    COPYBARA_INTEGRATE_REVIEW=https://github.com/google/adk-js/pull/3 from google:fix-build 2ec517cb5c06851d1225c9a59b6ac663affb55b5
    PiperOrigin-RevId: 802306153

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,11 +5,18 @@
  */
 
 import {defineConfig} from 'vitest/config';
+import path from 'path';
 
 export default defineConfig({
   test: {
     environment: 'node',
     globals: true,
     include: ['**/*_test.ts'],
+    
+  },
+  resolve: {
+    alias: {
+      '@google/adk': path.resolve(__dirname, './src')
+    }
   },
 });
