add9a18bbef0f3722a1eb310e42f133ce0b77028
Alexey Kalenkevich <kalenkevich@google.com>
2025-09-08 13:23:12 -0700

    Split entrypoints based on targets  - web  - node
    PiperOrigin-RevId: 804561701

---
+++ src/common.ts
@@ -0,0 +1,41 @@
+/**
+ * @license
+ * Copyright 2025 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+export {BaseAgent} from './agents/base_agent.js';
+export {CallbackContext} from './agents/callback_context.js';
+export {InvocationContext} from './agents/invocation_context.js';
+export {LiveRequestQueue} from './agents/live_request_queue.js';
+export type {LiveRequest} from './agents/live_request_queue.js';
+export {LlmAgent} from './agents/llm_agent.js';
+export {LoopAgent} from './agents/loop_agent.js';
+export {ParallelAgent} from './agents/parallel_agent.js';
+export {RunConfig} from './agents/run_config.js';
+export {SequentialAgent} from './agents/sequential_agent.js';
+export {InMemoryArtifactService} from './artifacts/in_memory_artifact_service.js';
+export type {BaseCredentialService} from './auth/credential_service/base_credential_service.js';
+export {Event} from './events/event.js';
+export type {EventActions} from './events/event_actions.js';
+export {InMemoryMemoryService} from './memory/in_memory_memory_service.js';
+export type {LlmRequest} from './models/llm_request.js';
+export {LlmResponse} from './models/llm_response.js';
+export {BasePlugin} from './plugins/base_plugin.js';
+export {LoggingPlugin} from './plugins/logging_plugin.js';
+export {PluginManager} from './plugins/plugin_manager.js';
+export {InMemoryRunner} from './runner/in_memory_runner.js';
+export {Runner} from './runner/runner.js';
+export {InMemorySessionService} from './sessions/in_memory_session_service.js';
+export {Session} from './sessions/session.js';
+export {AgentTool} from './tools/agent_tool.js';
+export {BaseToolset} from './tools/base_toolset.js';
+export {FunctionTool} from './tools/function_tool.js';
+export {GoogleSearchTool} from './tools/google_search_tool.js';
+export {ToolContext} from './tools/tool_context.js';
+export {zodObjectToSchema} from './utils/simple_zod_to_json.js';
+
+export * from './artifacts/base_artifact_service.js';
+export * from './memory/base_memory_service.js';
+export * from './sessions/base_session_service.js';
+export * from './tools/base_tool.js';
