1e189ffa20e8cfebe9f320c2588d5bf8d0ed9ca2
Alexey Kalenkevich <kalenkevich@google.com>
2026-01-08 13:41:48 -0800

    Add package name to the wombat registrty url
    PiperOrigin-RevId: 853868876

--- src/version.ts
+++ src/version.ts
@@ -1,8 +1,7 @@
 /**
  * @license
- * Copyright 2026 Google LLC
+ * Copyright 2025 Google LLC
  * SPDX-License-Identifier: Apache-2.0
  */
-
 // version: major.minor.patch
-export const version = '0.2.3';
+export const version = '0.2.1';
